1ce5dbb25689c8e428322eb6fb442dfd1f6b0310
lowejosh <lowejosh50@gmail.com>
2017-04-08 21:11:31 +1100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-//abc
+function login(showhide) {
+	if(showhide == "show") {
+	    document.getElementById('login-popup').style.visibility="visible";
+	} else if(showhide == "hide") {
+	    document.getElementById('login-popup').style.visibility="hidden"; 
+	}
+}
